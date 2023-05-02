7b54069da30fa16c0a3c184b544cf34ff6dc611b
vinit <vinitkkulkarni@gmail.com>
2023-05-02 19:08:03 +0530

    Refactored:code to validate password with minimum 1 numberic value

--- src/main/java/UserRegistrationMain.java
+++ src/main/java/UserRegistrationMain.java
@@ -43,7 +43,7 @@ public class UserRegistrationMain {
         return passwordValidation(password);
     }
     public static boolean passwordValidation(String value){
-        Pattern pattern = Pattern.compile("^(?=[1-9a-zA-Z])(?=.*[A-Z]).{8}");
+        Pattern pattern = Pattern.compile("^(?=[a-z])(?=.*[A-Z])(?=.*[0-9]).{8}$");
         Matcher matcher = pattern.matcher(value);
         return (matcher.matches());
     }
