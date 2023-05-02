831351b73bba3dc1a471811826b0bae54580d70f
vinit <vinitkkulkarni@gmail.com>
2023-05-02 18:23:40 +0530

    Added:initial commit message

---
+++ src/main/java/UserDetails.java
@@ -0,0 +1,16 @@
+
+public class UserDetails {
+    String firstName;
+    String lastName;
+    String emailId;
+    String mobileNumber;
+    String password;
+
+    public UserDetails(String firstName, String lastName, String emailId, String mobileNumber, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailId = emailId;
+        this.mobileNumber = mobileNumber;
+        this.password = password;
+    }
+}
---
+++ src/main/java/UserRegistrationMain.java
@@ -0,0 +1,6 @@
+
+public class UserRegistrationMain {
+    public static void main(String[] args) {
+        System.out.println("USER REGISTRATION WITH REGEX");
+    }
+}
